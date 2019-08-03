5323bedf85f639edfd03cbd36c219416e141f938
divyanshu59 <greatdivyanshu59@gmail.com>
2019-08-04 03:35:23 +0530

    Updated Database

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2019 at 02:52 PM
+-- Generation Time: Aug 04, 2019 at 12:04 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -32,8 +32,18 @@ CREATE TABLE `addons` (
   `id` int(11) NOT NULL,
   `name` varchar(1100) NOT NULL,
   `image` varchar(1100) NOT NULL,
-  `price` int(11) NOT NULL
+  `price` int(11) NOT NULL,
+  `catid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `addons`
+--
+
+INSERT INTO `addons` (`id`, `name`, `image`, `price`, `catid`) VALUES
+(6, 'qw', 'http://localhost/cycle_clinic/addons/img6.1563705418.jpg', 11, 2),
+(7, 'Cat 3', 'http://localhost/cycle_clinic/addons/happy.1564868466.jpg', 0, 2),
+(8, 'SAmple 2323', 'http://localhost/cycle_clinic/addons/img3.1564869236.jpg', 23232, 2);
 
 -- --------------------------------------------------------
 
@@ -61,6 +71,50 @@ INSERT INTO `admin` (`id`, `name`, `email`, `password`, `type`, `addon`, `status
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(1100) NOT NULL,
+  `image` text NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `image`, `status`) VALUES
+(1, 'Cat 1', 'http://localhost/cycle_clinic/img/7343-happy-person-raising-one-hand.png', 0),
+(2, 'Category 2', 'http://localhost/cycle_clinic/img/cry.png', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE `city` (
+  `id` int(11) NOT NULL,
+  `name` varchar(1100) NOT NULL,
+  `stateid` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `additional` varchar(1100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`id`, `name`, `stateid`, `status`, `additional`) VALUES
+(1, 'Bhimtal', 1, 1, NULL),
+(2, 'Haldwani', 1, 1, NULL),
+(3, 'Nainital', 1, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `service`
 --
 
@@ -80,18 +134,40 @@ CREATE TABLE `service` (
   `intresedNewBike` tinyint(1) NOT NULL,
   `sign` varchar(1100) DEFAULT NULL,
   `driverId` varchar(1100) DEFAULT NULL,
-  `trackingId` varchar(1100) DEFAULT NULL
+  `trackingId` varchar(1100) DEFAULT NULL,
+  `paymentMode` int(11) NOT NULL,
+  `city` varchar(1100) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `service`
 --
 
-INSERT INTO `service` (`id`, `uid`, `name`, `phone`, `type`, `service`, `address`, `date`, `scratches`, `addons`, `status`, `intresedlnsurence`, `intresedNewBike`, `sign`, `driverId`, `trackingId`) VALUES
-(6, 'jdqrI9zmG7MQHxLOJ93wAbb1mZy2', 'New customer', '+919328486441', 'Geared', 'Safety', 'B7 bhadralok bunglow\nborbhatha road\nankleshwar', 'Wed Jul 03 21:10:14 GMT+05:30 2019', 0, '[0, 2]', 3, 0, 0, NULL, NULL, NULL),
-(2, 'vNHRsKL7wpacjHoVXh2PXkqc2hF2', 'kartik', '+919328486441', 'Geared', 'Safety', 'b7 bhadralok bungalow\nborbhatha road\nAnkleshwar', 'Tue Jun 25 00:15:38 GMT+05:30 2019', 0, '[4, 2]', 5, 0, 1, NULL, NULL, NULL),
-(3, 'vNHRsKL7wpacjHoVXh2PXkqc2hF2', 'Margi patel', '+911234567890', 'Geared', 'Safety', 'somewhere\non\nearth', 'Tue Jun 25 03:25:37 GMT+05:30 2019', 0, '[]', 1, 0, 0, NULL, NULL, NULL),
-(7, 'cHYS6637zgQk1Muudlp8M1sRSKR2', 'police report', '9888499375', 'Geared', 'Safety', 'njkklooooo\nljhuiiiiiiioooh\nbbll', 'Mon Jul 01 20:25:57 GMT+05:30 2019', 0, '[3, 2, 1]', 0, 0, 0, NULL, NULL, NULL);
+INSERT INTO `service` (`id`, `uid`, `name`, `phone`, `type`, `service`, `address`, `date`, `scratches`, `addons`, `status`, `intresedlnsurence`, `intresedNewBike`, `sign`, `driverId`, `trackingId`, `paymentMode`, `city`) VALUES
+(6, 'jdqrI9zmG7MQHxLOJ93wAbb1mZy2', 'New customer', '+919328486441', 'Geared', 'Safety', 'B7 bhadralok bunglow\nborbhatha road\nankleshwar', 'Wed Jul 03 21:10:14 GMT+05:30 2019', 0, '[6,6,7]', 3, 0, 0, NULL, NULL, NULL, 0, ''),
+(2, 'vNHRsKL7wpacjHoVXh2PXkqc2hF2', 'kartik', '+919328486441', 'Geared', 'Safety', 'b7 bhadralok bungalow\nborbhatha road\nAnkleshwar', 'Tue Jun 25 00:15:38 GMT+05:30 2019', 0, '[4, 2]', 5, 0, 1, NULL, NULL, NULL, 0, ''),
+(3, 'vNHRsKL7wpacjHoVXh2PXkqc2hF2', 'Margi patel', '+911234567890', 'Geared', 'Safety', 'somewhere\non\nearth', 'Tue Jun 25 03:25:37 GMT+05:30 2019', 0, '[]', 1, 0, 0, NULL, NULL, NULL, 0, ''),
+(7, 'cHYS6637zgQk1Muudlp8M1sRSKR2', 'police report', '9888499375', 'Geared', 'Safety', 'njkklooooo\nljhuiiiiiiioooh\nbbll', 'Mon Jul 01 20:25:57 GMT+05:30 2019', 0, '[3, 2, 1]', 0, 0, 0, NULL, NULL, NULL, 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+CREATE TABLE `state` (
+  `id` int(11) NOT NULL,
+  `name` varchar(1100) NOT NULL,
+  `status` int(11) NOT NULL,
+  `additional` varchar(1100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`id`, `name`, `status`, `additional`) VALUES
+(1, 'Uttarakhand', 1, '');
 
 -- --------------------------------------------------------
 
@@ -149,6 +225,22 @@ INSERT INTO `user` (`id`, `name`, `email`, `address`, `phoneNumber`, `status`) V
 ('12cdwwf34', 'Kartik Patel', 'patelkartik1910@gmail.com', 'b7 bhadralok bunglow\nBorbhatha road\nAnkleshwar', '+916352122123', 0),
 ('jdqrI9zmG7MQHxLOJ93wAbb1mZy2', 'Kartik Patel', 'patelkartik1910@gmail.com', '', '', 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `worker`
+--
+
+CREATE TABLE `worker` (
+  `id` int(11) NOT NULL,
+  `name` varchar(1100) NOT NULL,
+  `email` varchar(1100) NOT NULL,
+  `phone` bigint(20) NOT NULL,
+  `password` varchar(1100) NOT NULL,
+  `photo` varchar(1100) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -166,9 +258,27 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `service`
 --
 ALTER TABLE `service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -184,6 +294,12 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `worker`
+--
+ALTER TABLE `worker`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -191,7 +307,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `addons`
 --
 ALTER TABLE `addons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `admin`
@@ -200,16 +316,40 @@ ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `service`
 --
 ALTER TABLE `service`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tracking`
 --
 ALTER TABLE `tracking`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `worker`
+--
+ALTER TABLE `worker`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
